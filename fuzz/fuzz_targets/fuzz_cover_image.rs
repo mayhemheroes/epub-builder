f16e591880105b039374bce2efb2f049b2001b0e
britton <brittonkfischer@gmail.com>
2023-05-09 18:41:30 -0700

    Implement Mayhem

---
+++ fuzz/fuzz_targets/fuzz_cover_image.rs
@@ -0,0 +1,20 @@
+use honggfuzz::fuzz;
+use epub_builder::{EpubBuilder, Result, ZipLibrary};
+
+fn try_fuzz(s: &str) -> Result<Vec<u8>> {
+    let mut output = Vec::<u8>::new();
+    EpubBuilder::new(ZipLibrary::new()?)?
+        .add_cover_image("cover.png", s.as_bytes(), "image/png")?
+        .generate(&mut output)?;
+    Ok(output)
+}
+
+fn main() {
+    loop {
+        fuzz!(|data: &[u8]| {
+            if let Ok(s) = std::str::from_utf8(data) {
+                let _ = try_fuzz(s);
+            }
+        });
+    }
+}
